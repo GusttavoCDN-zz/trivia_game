22c46110396a3c1c8bd4eaa0582fdb2914ff7ef3
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-06-20 10:46:43 -0300

    feat: Criado Widget inicial de tela de login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,29 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Widget from '../src/components/Widget';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const LoginContainer = styled.div`
+  align-items: center;
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <LoginContainer>
+      <Widget>
+        <Widget.Header>Trivia</Widget.Header>
+        <Widget.Content>
+          <p>
+            Teste seus conhecimentos com diversos quizzes personalizados e diverta-se
+            criando o seu proprio Quiz!
+          </p>
+          <Widget.form>
+            <input type="text" placeholder="Digite seu nome para jogar" />
+            <button type="button">Jogar</button>
+          </Widget.form>
+        </Widget.Content>
+      </Widget>
+    </LoginContainer>
+  );
 }
---
+++ src/components/Widget/index.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+const Widget = styled.section`
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 350px;
+`;
+
+Widget.Content = styled.div`
+  padding: 2rem;
+`;
+
+Widget.Header = styled.div`
+  border: 1px solid black;
+  display: flex;
+  font-size: 1.5rem;
+  justify-content: center;
+  padding: 1rem;
+`;
+
+Widget.form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  input,
+  button {
+    padding: 0.5rem;
+    margin: 0.5rem 0;
+  }
+`;
+
+export default Widget;
